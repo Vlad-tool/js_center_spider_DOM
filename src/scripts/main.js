965aa0eac157b89be3eccd5fd6d7916aca3baf18
Vlad <rebellqwe@gmail.com>
2026-02-27 23:04:38 +0300

    DOM spider solution corrected

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,21 @@
 'use strict';
 
-const spider = document.querySelector('.spider');
+document.addEventListener('DOMContentLoaded', () => {
+  const spider = document.querySelector('.spider');
+  const field = document.querySelector('.wall');
 
-spider.style.top = '50%';
-spider.style.left = '50%';
-spider.style.transform = 'translate(-50%, -50%)';
+  if (!field || !spider) {
+    return;
+  }
+
+  if (getComputedStyle(field).position === 'static') {
+    field.style.position = 'relative';
+  }
+
+  const topPosition = (field.clientHeight - spider.clientHeight) / 2;
+  const leftPosition = (field.clientWidth - spider.clientWidth) / 2;
+
+  spider.style.position = 'absolute';
+  spider.style.top = topPosition + 'px';
+  spider.style.left = leftPosition + 'px';
+});
